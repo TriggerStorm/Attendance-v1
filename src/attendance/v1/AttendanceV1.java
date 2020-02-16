2593e76e06c224e2e1b67e955a817d9e590593ec
TriggerStorm <michael.h.p@hotmail.com>
2020-02-16 13:20:08 +0100

    jfoenix + loin in as main
    login is main

--- src/attendance/v1/AttendanceV1.java
+++ src/attendance/v1/AttendanceV1.java
@@ -19,7 +19,7 @@ public class AttendanceV1 extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("gui/view/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("gui/view/logIn.fxml"));
         
         Scene scene = new Scene(root);
         
