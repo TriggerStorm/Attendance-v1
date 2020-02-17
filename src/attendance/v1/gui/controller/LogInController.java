7e73d49d63b39b36b372fbefcb2e1ee69611f8e2
TriggerStorm <michael.h.p@hotmail.com>
2020-02-17 14:18:14 +0100

    add login code
    can go from log in to student.

--- src/attendance/v1/gui/controller/LogInController.java
+++ src/attendance/v1/gui/controller/LogInController.java
@@ -5,11 +5,18 @@
  */
 package attendance.v1.gui.controller;
 
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,6 +29,8 @@ public class LogInController implements Initializable {
     private TextField TF_email;
     @FXML
     private TextField TF_pasword;
+    @FXML
+    private JFXButton Bn_login;
 
     /**
      * Initializes the controller class.
@@ -30,5 +39,25 @@ public class LogInController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handle_login(ActionEvent event) throws IOException {
+       
+        Parent root1;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/attendance/v1/gui/view/Student.fxml"));
+        root1 = (Parent) fxmlLoader.load();
+        
+        fxmlLoader.<StudentController>getController();
+
+        Stage addStage = new Stage();
+        Scene addScene = new Scene(root1);
+
+        
+        addStage.setScene(addScene);
+        addStage.show();
+        
+        Stage stage = (Stage) Bn_login.getScene().getWindow();
+        stage.close();
+    }
     
 }
---
+++ src/attendance/v1/gui/controller/StudentController.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendance.v1.gui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Trigger
+ */
+public class StudentController implements Initializable {
+
+    @FXML
+    private Label date;
+    @FXML
+    private AnchorPane header;
+    @FXML
+    private Label title;
+    @FXML
+    private Button Bn_SDE;
+    @FXML
+    private Button Bn_SCO;
+    @FXML
+    private Button Bn_ITO;
+    @FXML
+    private Button Bn_DBOS;
+    @FXML
+    private AnchorPane body1;
+    @FXML
+    private Label header2;
+    @FXML
+    private TextField message;
+    @FXML
+    private TextField message1;
+    @FXML
+    private TextField attendance;
+    @FXML
+    private Button button;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleButtonAction(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    }
+    
+}
