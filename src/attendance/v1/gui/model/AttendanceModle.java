36e863c345cd7c586bfb65346473351e091eb8c4
TriggerStorm <michael.h.p@hotmail.com>
2020-02-18 21:07:24 +0100

    Update AttendanceModle.java
    wq

--- src/attendance/v1/gui/model/AttendanceModle.java
+++ src/attendance/v1/gui/model/AttendanceModle.java
@@ -5,11 +5,54 @@
  */
 package attendance.v1.gui.model;
 
+import attendance.v1.bll.BllManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 /**
  *
  * @author Trigger
  */
 public class AttendanceModle {
-    private BllManager = new bllManager;
+    private BllManager bllManager;
+    private ObservableList<AttSco> ScoList;
+    private ObservableList<AttSde> SdeList;
+    private ObservableList<AttIto> ItoList;
+    private ObservableList<AttDB> DBList;
+
+    public AttendanceModle() {
+        bllManager = new BllManager();
+        getScoAttendance();
+    }
+    
+    public ObservableList<AttSco> getScoAttendance(){
+    
+      List<Sco> allSco = bllManager.fetchAllSco();
+      ScoList = FXCollections.observableArrayList(allSco);
+      return ScoList;
+        
+    }
+    public ObservableList<AttSco> getScoAttendance(){
+    
+      List<Sco> allSco = bllManager.fetchAllSco();
+      SdeList = FXCollections.observableArrayList(allSco);
+      return SdeList;
+        
+    }
+    public ObservableList<AttSco> getScoAttendance(){
+    
+      List<Sco> allSco = bllManager.fetchAllSco();
+      ScoList = FXCollections.observableArrayList(allSco);
+      return ScoList;
+        
+    }
+    public ObservableList<AttSco> getScoAttendance(){
+    
+      List<Sco> allSco = bllManager.fetchAllSco();
+      ScoList = FXCollections.observableArrayList(allSco);
+      return ScoList;
+        
+    }
+    
     
 }
