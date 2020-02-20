f8c204ec280a713111b443557f9ce5c268ed6afa
filipus30 <56824843+filipus30@users.noreply.github.com>
2020-02-20 03:00:18 +0100

    random 4digits code

--- src/attendance/v1/gui/controller/GeneratedCodeController.java
+++ src/attendance/v1/gui/controller/GeneratedCodeController.java
@@ -6,7 +6,9 @@
 package attendance.v1.gui.controller;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
@@ -26,7 +28,21 @@ public class GeneratedCodeController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       // String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?";
+       // String pwd = RandomStringUtils.random( 15, characters );
+      //  System.out.println( pwd );
+
+     String password = new Random().ints(10, 33, 122).mapToObj(i -> String.valueOf((char)i)).collect(Collectors.joining());
+ String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 4) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+ 
+    TF_code.setText(saltStr);
     }    
     
 }
