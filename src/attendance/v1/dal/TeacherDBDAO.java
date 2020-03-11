d4e7baeaa302861e8b20ba86e565a047d44beee7
filipus30 <56824843+filipus30@users.noreply.github.com>
2020-03-11 23:39:34 +0100

    .
    .

--- src/attendance/v1/dal/TeacherDBDAO.java
+++ src/attendance/v1/dal/TeacherDBDAO.java
@@ -5,10 +5,32 @@
  */
 package attendance.v1.dal;
 
+import attendance.v1.be.Classes;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author macos
  */
 public class TeacherDBDAO {
-    
+     private DBConnection db;
+     public Classes assignTeacherClass(Classes classes, String teacher) throws SQLException 
+    {
+        db = new DBConnection();
+        
+           
+        try(Connection con = db.getConnection()){
+            String SQLStmt = "UPDATE SUBJECTS SET AssociatedTeacher = ? WHERE id = ?;";
+            
+            PreparedStatement pstmt = con.prepareStatement(SQLStmt);
+             pstmt.setString(1,teacher);
+            pstmt.setInt(2,classes.getClasskey());
+        }
+        return new Classes(classes.getClasskey(),classes.getClassName(),classes.getClassIMG(),classes.getAssociatedCourse(),teacher);
+}
 }
