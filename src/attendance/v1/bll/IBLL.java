36b358f7f50e7beb0676d82fc3d344a63024bf29
cilledyr <47023263+cilledyr@users.noreply.github.com>
2020-03-13 22:41:46 +0100

    Looked over user in dal layer. Login works, if you fetch valid users from database

--- src/attendance/v1/bll/IBLL.java
+++ src/attendance/v1/bll/IBLL.java
@@ -31,8 +31,8 @@ public interface IBLL {
 // UsersDBDAO methods
     public List<User> getAllUsers();
     public User getUser(int userKey);
-    public User addNewUserToDB(String userName, String password, String email, int phoneNr, String address, int postCode, String city, String teacher, String userIMG);
-    public User editUser (User userToEdit, String userName, String password, String email, int phoneNr, String address, int postCode, String city, String teacher, String userIMG);
+    public User addNewUserToDB(String userName, String password, String email, int phoneNr, String address, int postCode, String city, boolean teacher, String userIMG);
+    public User editUser (User userToEdit, String userName, String password, String email, int phoneNr, String address, int postCode, String city, boolean teacher, String userIMG);
     public void removeUserFromDB(User userToDelete);
     public int checkUserLogin (String email, String password);
     public boolean checkIfTeacher(String email);
@@ -40,4 +40,6 @@ public interface IBLL {
  // AttendanceDBDAO methods
     public List<Attendance> getAllAttendances();
 
+    
+    public List<Attendance> getStudentAttendanceInSubject(int studentKey, int subjectKey);
 }
