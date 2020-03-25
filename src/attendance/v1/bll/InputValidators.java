e6bd789b310961a2803a572dedecdf8e01582156
SnowBoy73 <alan4999@gmail.com>
2020-03-25 15:23:47 +0100

    Validator tested and working
    Proetty robust. Not bulletproof though.

--- src/attendance/v1/bll/InputValidators.java
+++ src/attendance/v1/bll/InputValidators.java
@@ -16,73 +16,37 @@ package attendance.v1.bll;
  */
 
 public class InputValidators  implements Serializable {
-    private static final int MAX_EMAIL_LENGTH = 64;
-    private static final String SPECIAL_CHARS = "\\p{Cntrl}\\(\\)<>@,;:'\\\\\\\"\\.\\[\\]";
-    private static final String VALID_CHARS = "(\\\\.)|[^\\s" + SPECIAL_CHARS + "]";
-    private static final String QUOTED_USER = "(\"(\\\\\"|[^\"])*\")";
-    private static final String WORD = "((" + VALID_CHARS + "|')+|" + QUOTED_USER + ")";
-    private static final String EMAIL_REGEX = "^\\s*" + WORD + "(\\." + WORD + ")*$";
-    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
-
-    private static final int PHONE_NUMBER_LENGTH = 8;
-    private static final int POSTCODE_LENGTH = 4;
-    private static final int MIN_ADDRESS_LENGTH = 4;
-    private static final int MAX_ADDRESS_LENGTH = 64;
-    private static final int MIN_CITY_LENGTH = 2;
-
+   
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX = 
+    Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
     
     
-    public boolean isValidEmail(String email) {
-    //  Returns true if email is less than 64 chars long and follows the email pattern
-    
-    String[] parts1 = email.split("@");
-    String username = parts1[0];
-    if (!IsValidWordWithNumbers(username)) {
-        return false;
-    }
-   // String rest = parts[1];
-    String parts2[] = parts1[1].split(".");
-    String domain = parts2[0];
-    if (!IsValidWordWithNumbers(domain)) {
-        return false;
-    }
-    String suffix = parts2[1];
-    if (!IsValidWordWith(suffix)) {
-        return false;
-    }
-    return true;
-     /*   if (email == null || email.length() > MAX_EMAIL_LENGTH) {
-            return false;
-        }
-        return EMAIL_PATTERN.matcher(email).matches();*/
      
+
+    public boolean isValidEmail(String email) {
+    //  Returns true if email string is is in valid email format. Works pretty well. Not 100% (eg: abc@xyz..com)
+    
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(email);
+        return matcher.find();
     }
 
-    
+
     public boolean isValidPhoneNumber(String phoneNumber) {
     //  Returns true if phoneNumber is 8 chars long and contains only numbers
-        if (phoneNumber.length() != PHONE_NUMBER_LENGTH) {
-            return false;
-        }
-        for (int i = 0; i < PHONE_NUMBER_LENGTH; i++) {
-            char ch = phoneNumber.charAt(i);
-              if (!Character.isDigit(ch)){
-            return false;
-            }
-        }
-        return true;
+        return isValidNumber(phoneNumber, 8);
     }
      
     
     public boolean isValidAddress(String address) {
     //  Returns true if address is between 4 and 64 chars long and contains at least 2 letters (numbers are not essential)
+        address = address.trim();
         int hasLetters = 0;
-        if ((address.length() < MIN_ADDRESS_LENGTH) && (address.length() > MAX_ADDRESS_LENGTH)) {
+        if ((address.length() < 4) && (address.length() > 64)) {
             return false;
         }
         for (int i = 0; i < address.length(); i++) {
             char ch = address.charAt(i);
-            if ((!Character.isLetterOrDigit(ch)) || (!Character.isSpaceChar(ch))) {
+            if (!(Character.isLetterOrDigit(ch) || Character.isSpaceChar(ch))) {
                 return false;
             } else {
                 if (Character.isLetter(ch)) {
@@ -99,91 +63,86 @@ public class InputValidators  implements Serializable {
     
     public boolean isValidPostCode(String postCode) {
     //  Returns true if postCode is 4 chars long and contains only numbers
-        if (postCode.length() != POSTCODE_LENGTH) {
+        return isValidNumber(postCode, 4);
+    }
+
+    
+    public boolean isValidCity(String city) {
+    //  Returns true if city is between 2 and 64 chars long and contains only letters
+        return areValidWordsWithSpace(city, 2, 64);
+    }
+    
+    
+    
+    
+    
+    
+    
+// Test word valididator methods
+    
+    public boolean isValidWord(String word, int min, int max) {  // not yet used
+       //  Returns true if word is between 1 and 64 chars long and contains only letters
+        word = word.trim();
+        if ((word.length() < min) || (word.length() > max)) {
             return false;
         }
-        for (int i = 0; i < POSTCODE_LENGTH; i++) {
-            char ch = postCode.charAt(i);
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+              if (!Character.isLetter(ch)){
+            return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean isValidWordWithNumbers(String word, int min, int max) {  // not yet used
+       //  Returns true if word is between 1 and 64 chars long and contains only letters and/or numbers
+        word = word.trim();
+        if ((word.length() < min) || (word.length() > max)) {
+            return false;
+        }
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+              if (!Character.isLetterOrDigit(ch)){
+            return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    public boolean areValidWordsWithSpace(String word, int min, int max) {  // not yet used
+       //  Returns true if word is between 1 and 64 chars long and contains only letters or spaces
+        word = word.trim();
+        if ((word.length() < min) || (word.length() > max)) {
+            return false;
+        }
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (!(Character.isLetter(ch) || Character.isSpaceChar(ch))) {
+            return false;
+            }
+        }
+        return true;
+    }
+    
+    
+   public boolean isValidNumber(String word, int noOfChars) {  // not yet used
+       //  Returns true if word is between 1 and 64 chars long and contains only letters
+        word = word.trim();
+        if (!(word.length() == noOfChars)) {
+            return false;
+        }
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
               if (!Character.isDigit(ch)){
             return false;
             }
         }
         return true;
     }
-
-    
-    public boolean isValidCity(String city) {
-    //  Returns true if city is between 2 and 64 chars long and contains only letters
-        if (city.length() < MIN_CITY_LENGTH) {
-            return false;
-        }
-        for (int i = 0; i < city.length(); i++) {
-            char ch = city.charAt(i);
-              if (!Character.isLetter(ch)){
-            return false;
-            }
-        }
-        return true;
-    }
-    
-     public boolean IsValidWordWithNumbers(String word) {  // not yet used
-       //  Returns true if word is between 1 and 64 chars long and contains only letters and/or numbers
-        if ((word.length() < 1) && (word.length() > MAX_EMAIL_LENGTH)) {
-            return false;
-        }
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-              if (!Character.isLetterOrDigit(ch)){
-            return false;
-            }
-        }
-        return true;
-    }
-    
-    
-     public boolean IsValidWordWith(String word) {  // not yet used
-       //  Returns true if word is between 1 and 64 chars long and contains only letters
-        if ((word.length() < 1) && (word.length() > MAX_EMAIL_LENGTH)) {
-            return false;
-        }
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-              if (!Character.isLetter(ch)){
-            return false;
-            }
-        }
-        return true;
-    }
-    
-    public boolean IsValidWordWithNumbers(String word) {  // not yet used
-       //  Returns true if word is between 1 and 64 chars long and contains only letters and/or numbers
-        if ((word.length() < 1) && (word.length() > MAX_EMAIL_LENGTH)) {
-            return false;
-        }
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-              if (!Character.isLetterOrDigit(ch)){
-            return false;
-            }
-        }
-        return true;
-    }
-    
-    
-     public boolean IsValidWordWith(String word) {  // not yet used
-       //  Returns true if word is between 1 and 64 chars long and contains only letters
-        if ((word.length() < 1) && (word.length() > MAX_EMAIL_LENGTH)) {
-            return false;
-        }
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-              if (!Character.isLetter(ch)){
-            return false;
-            }
-        }
-        return true;
-    }
-    
-     
+   
+  
+  
 }
     
